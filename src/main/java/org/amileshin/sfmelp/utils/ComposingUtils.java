88cd5d690f7d934d1775fc27ea9e95d7a627d872
amileshin <arbitmilka2000@gmail.com>
2024-05-28 15:41:10 +0300

    Fix ComposingUtils

--- src/main/java/org/amileshin/sfmelp/utils/ComposingUtils.java
+++ src/main/java/org/amileshin/sfmelp/utils/ComposingUtils.java
@@ -1,14 +1,16 @@
 package org.amileshin.sfmelp.utils;
 
-import org.amileshin.sfmelp.model.dto.ConnectInfoDTO;
+import org.amileshin.sfmelp.model.dto.ConnectionDB;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ComposingUtils {
+    public static final String CLEAR_DATABASE_CACHE = "SYSTEM DROP UNCOMPRESSED CACHE";
+
     private ComposingUtils() {}
 
-    public static String getDatabaseUrlFromConnectInfoDTO(String databaseName, ConnectInfoDTO dto) {
+    public static String getDatabaseUrlFromConnectInfoDTO(String databaseName, ConnectionDB dto) {
         return "jdbc:" + databaseName.toLowerCase() + "://" + dto.getHost() + ":" + dto.getPort()
                 + (dto.getDatabase().isBlank()? "" : "/" + dto.getDatabase());
     }
@@ -26,9 +28,5 @@ public class ComposingUtils {
 
     public static String getSQLRequestToGetByteForTable(String database, String table) {
         return "SELECT total_bytes FROM system.tables WHERE database = '" + database + "' AND name = '" + table + "'";
-    }
-
-    public static String getSQLRequestToClearDatabaseCache() {
-        return "SYSTEM DROP UNCOMPRESSED CACHE";
     }
 }
